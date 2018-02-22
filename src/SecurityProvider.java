20610d5a0950024e1dd5aef0b9844905b0daa0cf
Abhishek Joshi <abhishek.joshi20@gmail.com>
2018-02-22 05:22:53 -0500

    Cleanup, Readme, Junk Removed. Working on two devices on my side

--- src/SecurityProvider.java
+++ src/SecurityProvider.java
@@ -1,13 +1,10 @@
 package com.abhishekjoshi.updatesecurityprovider;
 
-import android.content.ContentValues;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.security.ProviderInstaller;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -16,24 +13,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SecurityProvider extends CordovaPlugin implements ProviderInstaller.ProviderInstallListener{
-    AlertDialog alertDialog;
     private static final int ERROR_DIALOG_REQUEST_CODE = 1;
-    public static final String INSTALL_IF_NEEDED = "installIfNeededAsync";
-    protected static CallbackContext _callbackContext = null;
-    private boolean mRetryProviderInstall;
-    private ContentValues returnValues;
-    private static final String TAG = "NotificationHub";
+    private static final String INSTALL_IF_NEEDED = "installIfNeededAsync";
+    private static CallbackContext _callbackContext = null;
+    private Activity _activity;
+    private SecurityProvider _sp;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         _callbackContext = callbackContext;
+        _sp = this;
 
         if (INSTALL_IF_NEEDED.equals(action)) {
-            ProviderInstaller.installIfNeededAsync(this.cordova.getActivity().getApplicationContext(), this);
+            this._activity = this.cordova.getActivity();
+            this.cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run(){
+                    ProviderInstaller.installIfNeededAsync(_activity,_sp);
+                }
+            });
         }
 
         return true;
     }
+
     /**
      * This method is only called if the provider is successfully updated
      * (or is already up-to-date).
@@ -41,9 +43,10 @@ public class SecurityProvider extends CordovaPlugin implements ProviderInstaller
     @Override
     public void onProviderInstalled(){
         // Provider is up-to-date, app can make secure network calls.
-        this.x("onProviderInstalled");
-        getCallbackContext().success("Security provider is successfully updated or is already up to date");
-
+        // getCallbackContext().success("Security provider is successfully updated or is already up to date");
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, this.result("installed"));
+        pluginResult.setKeepCallback(true);
+        getCallbackContext().sendPluginResult(pluginResult);
     }
 
     /**
@@ -52,73 +55,52 @@ public class SecurityProvider extends CordovaPlugin implements ProviderInstaller
      */
     @Override
     public void onProviderInstallFailed(int errorCode, Intent recoveryIntent) {
-        if (GooglePlayServicesUtil.isUserRecoverableError(errorCode)) {
-            // Recoverable error. Show a dialog prompting the user to
-            // install/update/enable Google Play services.
-            getCallbackContext().error("Google Play services is not available, please install.");
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
 
-            GooglePlayServicesUtil.showErrorDialogFragment(
+        if (api.isUserResolvableError(errorCode)) {
+            // Recoverable error. Send data to the app to indicate to
+            // install/update/enable Google Play services.
+
+            // getCallbackContext().error("Google Play services is not available, please install.");
+
+
+            // TODO: Only for debugging so I can see what's going on. To be removed.
+            api.showErrorDialogFragment(this.cordova.getActivity(),
                     errorCode,
-                    this.cordova.getActivity(),
                     ERROR_DIALOG_REQUEST_CODE,
                     new DialogInterface.OnCancelListener() {
                         @Override
                         public void onCancel(DialogInterface dialog) {
                             // The user chose not to take the recovery action
-                            onProviderInstallerNotAvailable();
                         }
                     });
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, this.result("action_required"));
+            pluginResult.setKeepCallback(true);
+            getCallbackContext().sendPluginResult(pluginResult);
+
         } else {
             // Google Play services is not available.
-            onProviderInstallerNotAvailable();
+            // This is reached if the provider cannot be updated for some reason.
+            // App should consider all HTTP communication to be vulnerable, and take
+            // appropriate action.
 
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ERROR_DIALOG_REQUEST_CODE) {
-            // Adding a fragment via GooglePlayServicesUtil.showErrorDialogFragment
-            // before the instance state is restored throws an error. So instead,
-            // set a flag here, which will cause the fragment to delay until
-            // onPostResume.
-            mRetryProviderInstall = true;
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, this.result("not_available"));
+            pluginResult.setKeepCallback(true);
+            getCallbackContext().sendPluginResult(pluginResult);
         }
     }
 
     /**
-     * On resume, check to see if we flagged that we need to reinstall the
-     * provider.
+     * Build JSON to return to the plugin's callback
      */
-    protected void onPostResume() {
-        if (mRetryProviderInstall) {
-            // We can now safely retry installation.
-            ProviderInstaller.installIfNeededAsync(this.cordova.getActivity(), this);
+    public JSONObject result(String result){
+        JSONObject returnResult = new JSONObject();
+        try{
+            returnResult.put("providerInstalledUpdated", result);
+        }catch(JSONException e){
+            getCallbackContext().error("Fatal error" + e.getMessage());
         }
-        mRetryProviderInstall = false;
-    }
-
-    private void onProviderInstallerNotAvailable() {
-        // This is reached if the provider cannot be updated for some reason.
-        // App should consider all HTTP communication to be vulnerable, and take
-        // appropriate action.
-
-        this.x("onProviderInstallerNotAvailable");
-    }
-    public void x(String xx){
-        this.alertDialog = new AlertDialog.Builder(this.cordova.getActivity()).create();
-        this.alertDialog.setTitle("Alert");
-        this.alertDialog.setMessage(xx);
-
-        this.alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        this.alertDialog.show();
+        return returnResult;
     }
 
     /**
--- www/SecurityProviderPlugin.js
+++ www/SecurityProviderPlugin.js
@@ -1,32 +1,23 @@
-
-    /*
-     * Unless required by applicable law or agreed to in writing,
-     * software distributed under the License is distributed on an
-     * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-     * KIND, either express or implied.  See the License for the
-     * specific language governing permissions and limitations
-     * under the License.
-     *
-     */
-
     var exec = require('cordova/exec');
-    var Promise = require('promise');
 
-    SecurityProvider.prototype.unregisterApplicationAsync = function ()
-    {
-        var deferral = new Promise.Deferral(),
+    var SecurityProvider = {
+        installIfNeededAsync: function(){
+            var p = new Promise(function(resolve, reject) {
+                        cb = function (result) {
+                            if(result.providerInstalledUpdated === "not_available" || result.providerInstalledUpdated === "action_required"){
+                                reject(result);
+                            }
+                            else if(result.providerInstalledUpdated === "installed"){
+                                resolve(result);
+                            }
+                        }
+                        exec(cb, cb, 'SecurityProvider', 'installIfNeededAsync', [""]);
+                    }
+                );
 
-            successCallback = function (result) {
-                deferral.resolve(result);
-            },
+            return p;
+        }
 
-            errorCallback = function (err) {
-                deferral.reject(err);
-            };
-
-        exec(successCallback, errorCallback, 'SecurityProvider', 'installIfNeededAsync', [this.notificationHubPath, this.connectionString]);
-
-        return deferral.promise;
-    }
+      };
 
     module.exports = SecurityProvider;
